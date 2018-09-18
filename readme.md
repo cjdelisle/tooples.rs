9d20a71dc393adea1b519641817627d6d495d4b2
Caleb James DeLisle <cjd@cjdns.fr>
2018-09-18 17:18:04 +0200

    Improvements, yay

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://travis-ci.org/cjdelisle/tooples.rs.svg?branch=master)](https://travis-ci.org/cjdelisle/tooples.rs)
 
-Array functions for tuples.
+## Array Functions
 
 In dynamic languages like javascript, you can just make an array of any types of elements
 and then push and pop anything you want, to and from it. Of course, if ever you reference
@@ -42,3 +42,65 @@ println!("{:?}", x);
 let x = x.shift().shift();
 println!("{:?}", x);
 ```
+
+## apply_to()
+
+This library also provides apply functions, you can call a function given a tuple of arguments.
+It provides `apply_to()`, `apply_to_mut()` and `apply_to_once()` which take a `Fn`, `FnMut` and
+`FnOnce` respectively. In every case, the tuple provided is by-val, it is consumed by the call
+and can nolonger be used again.
+
+Example:
+
+```rust
+let x = |a,b,c| {
+    println!("{} / {} / {}", a, b, c);
+};
+
+let y = ();
+let y = y.push(1);
+let y = y.push(&"hello");
+let y = y.push(3.2);
+
+y.apply_to(&x);
+```
+
+## Advanced usage
+
+You should probably define your functions by the precise tuples which they expect to take, however,
+if you want to define a generic function that will take ANY tuple (up to 12 elements) and push one
+particular element to the end of it, you can do so like this.
+
+```rust
+use std::time::Instant;
+fn push_current_time<W>(w:W) -> Push<W,Instant> where W:CanPush<Instant> {
+    w.push(Instant::now())
+}
+fn main() {
+    let x = ();
+    let x = x.push(3);
+    let x = push_current_time(x);
+    let x = x.push(&"hi");
+    let x = push_current_time(x);
+    println!("{:?}", x);
+}
+```
+
+Here, instead of specifying the exact type of input tuple, you specify that it is anything which is
+"capable of having an Instant pushed to it" (`where W:CanPush<Instant>`). The return type is
+described as "the result of pushing Instant to a W" (`Push<W,Instant>`). You can even get more
+complicated than that, but the function signatures will quickly get out of hand.
+
+```rust
+fn replace_last_item<W,X>(w:W, x:X) -> Push<Pop<W>,X> where
+    W:CanPop, Pop<W>:CanPush<X>
+{
+    let w = w.pop();
+    w.push(x)
+}
+```
+
+In this example, we accept a tuple W, or more exactly, we accept a W that "can be popped from"
+(`W:CanPop`) as long as the result of popping W must be able to have an X pushed to it
+(`Pop<W>:CanPush<X>`). This function will replace the last item in a tuple, no matter what length
+or types it contains.
--- src/lib.rs
+++ src/lib.rs
@@ -44,56 +44,91 @@ pub trait CanUnshift<X> {
 }
 pub type Unshift<W,X> = <W as CanUnshift<X>>::Result;
 
-macro_rules! mk_pushpop {
-    ($firstlet:ident ; [$($letter:ident),*] ; $lastlet:ident ; $firstnum:tt ; [$($number:tt),*] ; $lastnum:tt) => {
 
-        impl<X,$firstlet, $($letter,)* $lastlet> CanPush<X>
-            for ($firstlet, $($letter,)* $lastlet)
-        {
-            type Result = ($firstlet, $($letter,)* $lastlet, X);
-            fn push(self, val: X) -> Self::Result {
-                ( self.$firstnum, $(self.$number,)* self.$lastnum, val )
-            }
-        }
-
-        impl<$firstlet, $($letter,)* $lastlet> CanPop
-            for ($firstlet, $($letter,)* $lastlet)
-        {
-            type Result = ($firstlet, $($letter,)*);
-            fn pop(self) -> Self::Result {
-                ( self.$firstnum, $(self.$number,)* )
-            }
-        }
-        impl<$firstlet, $($letter,)* $lastlet> CanShift
-            for ($firstlet, $($letter,)* $lastlet)
-        {
-            type Result = ($($letter,)* $lastlet);
-            fn shift(self) -> Self::Result {
-                ( $(self.$number,)* self.$lastnum )
-            }
-        }
-
-        impl<X,$firstlet, $($letter,)* $lastlet> CanUnshift<X>
-            for ($firstlet, $($letter,)* $lastlet)
-        {
-            type Result = (X, $firstlet, $($letter,)* $lastlet);
-            fn unshift(self, val: X) -> Self::Result {
-                ( val, self.$firstnum, $(self.$number,)* self.$lastnum )
-            }
-        }
-    }
+pub trait CanApply<F>: CanApplyMut<F> {
+    fn apply_to(self, f:&F) -> Self::Output;
 }
-mk_pushpop!(A ; [] ; B ; 0 ; [] ; 1);
-mk_pushpop!(A ; [B] ; C ; 0 ; [1] ; 2);
-mk_pushpop!(A ; [B,C] ; D ; 0 ; [1,2] ; 3);
-mk_pushpop!(A ; [B,C,D] ; E ; 0 ; [1,2,3] ; 4);
-mk_pushpop!(A ; [B,C,D,E] ; F ; 0 ; [1,2,3,4] ; 5);
-mk_pushpop!(A ; [B,C,D,E,F] ; G ; 0 ; [1,2,3,4,5] ; 6);
-mk_pushpop!(A ; [B,C,D,E,F,G] ; H ; 0 ; [1,2,3,4,5,6] ; 7);
-mk_pushpop!(A ; [B,C,D,E,F,G,H] ; I ; 0 ; [1,2,3,4,5,6,7] ; 8);
-mk_pushpop!(A ; [B,C,D,E,F,G,H,I] ; J ; 0 ; [1,2,3,4,5,6,7,8] ; 9);
-mk_pushpop!(A ; [B,C,D,E,F,G,H,I,J] ; K ; 0 ; [1,2,3,4,5,6,7,8,9] ; 10);
-mk_pushpop!(A ; [B,C,D,E,F,G,H,I,J,K] ; L ; 0 ; [1,2,3,4,5,6,7,8,9,10] ; 11);
+pub trait CanApplyMut<F>: CanApplyOnce<F> {
+    fn apply_to_mut(self, f:&mut F) -> Self::Output;
+}
+pub trait CanApplyOnce<F> {
+    type Output;
+    fn apply_to_once(self, f:F) -> Self::Output;
+}
+
+macro_rules! mk_funcs {
+    (
+        TWO;
+        $firstlet:ident;
+        [$($letter:ident),*];
+        $lastlet:ident;
+        $firstnum:tt;
+        [$($number:tt),*];
+        $lastnum:tt
+    ) => {
+        mk_funcs!(
+            ONE;
+            [$firstlet, $($letter,)* $lastlet];
+            [$($letter,)* $lastlet];
+            [$firstlet $(,$letter)*];
+            [$firstnum, $($number,)* $lastnum];
+            [$($number,)* $lastnum];
+            [$firstnum $(,$number)*]
+        );
+    };
+    (
+        ONE;
+        [$($letters:ident),*];
+        [$($lnofirst:ident),*];
+        [$($lnolast:ident),*];
+        [$($numbers:tt),*];
+        [$($nnofirst:tt),*];
+        [$($nnolast:tt),*]
+    ) => {
+        impl<X,$($letters,)*> CanPush<X> for ($($letters,)*) {
+            type Result = ($($letters,)* X,);
+            fn push(self, val: X) -> Self::Result { ( $(self.$numbers,)* val, ) }
+        }
+        impl<$($letters,)*> CanPop for ($($letters,)*) {
+            type Result = ($($lnolast,)*);
+            fn pop(self) -> Self::Result { ( $(self.$nnolast,)* ) }
+        }
+        impl<$($letters,)*> CanShift for ($($letters,)*) {
+            type Result = ($($lnofirst,)*);
+            fn shift(self) -> Self::Result { ( $(self.$nnofirst,)* ) }
+        }
+        impl<X,$($letters,)*> CanUnshift<X> for ($($letters,)*) {
+            type Result = (X, $($letters,)*);
+            fn unshift(self, val: X) -> Self::Result {
+                ( val, $(self.$numbers,)* )
+            }
+        }
+
+        impl<Fun,Z,$($letters,)*> CanApply<Fun> for ($($letters,)*) where Fun:Fn($($letters,)*)->Z {
+            fn apply_to(self, f:&Fun) -> Self::Output { f($(self.$numbers,)*) }
+        }
+        impl<Fun,Z,$($letters,)*> CanApplyMut<Fun> for ($($letters,)*) where Fun:FnMut($($letters,)*)->Z {
+            fn apply_to_mut(self, f:&mut Fun) -> Self::Output { f($(self.$numbers,)*) }
+        }
+        impl<Fun,Z,$($letters,)*> CanApplyOnce<Fun> for ($($letters,)*) where Fun:FnOnce($($letters,)*)->Z {
+            type Output = Z;
+            fn apply_to_once(self, f:Fun) -> Self::Output { f($(self.$numbers,)*) }
+        }
+    };
+}
+mk_funcs!(ONE; []; []; []; []; []; []); // zero items
+mk_funcs!(ONE; [A]; []; []; [0]; []; []); // one item
+mk_funcs!(TWO; A ; [] ; B ; 0 ; [] ; 1);
+mk_funcs!(TWO; A ; [B] ; C ; 0 ; [1] ; 2);
+mk_funcs!(TWO; A ; [B,C] ; D ; 0 ; [1,2] ; 3);
+mk_funcs!(TWO; A ; [B,C,D] ; E ; 0 ; [1,2,3] ; 4);
+mk_funcs!(TWO; A ; [B,C,D,E] ; F ; 0 ; [1,2,3,4] ; 5);
+mk_funcs!(TWO; A ; [B,C,D,E,F] ; G ; 0 ; [1,2,3,4,5] ; 6);
+mk_funcs!(TWO; A ; [B,C,D,E,F,G] ; H ; 0 ; [1,2,3,4,5,6] ; 7);
+mk_funcs!(TWO; A ; [B,C,D,E,F,G,H] ; I ; 0 ; [1,2,3,4,5,6,7] ; 8);
+mk_funcs!(TWO; A ; [B,C,D,E,F,G,H,I] ; J ; 0 ; [1,2,3,4,5,6,7,8] ; 9);
+mk_funcs!(TWO; A ; [B,C,D,E,F,G,H,I,J] ; K ; 0 ; [1,2,3,4,5,6,7,8,9] ; 10);
+mk_funcs!(TWO; A ; [B,C,D,E,F,G,H,I,J,K] ; L ; 0 ; [1,2,3,4,5,6,7,8,9,10] ; 11);
 
 
 #[cfg(test)]
@@ -105,12 +140,52 @@ mod tests {
     fn do_shift<W>(w:W) -> Shift<W> where W:CanShift { w.shift() }
     fn do_unshift<W,X>(w:W, x:X) -> Unshift<W,X> where W:CanUnshift<X> { w.unshift(x) }
 
+    fn do_shift_unshift<W,X>(w:W, x:X) -> Unshift<Shift<W>,X> where
+        W:CanShift, Shift<W>:CanUnshift<X>
+    {
+        let w = w.shift();
+        w.unshift(x)
+    }
+
+    fn replace_last_item<W,X>(w:W, x:X) -> Push<Pop<W>,X> where
+        W:CanPop, Pop<W>:CanPush<X>
+    {
+        let w = w.pop();
+        w.push(x)
+    }
+
+    use std::time::Instant;
+    fn push_current_time<W>(w:W) -> Push<W,Instant> where W:CanPush<Instant> {
+        w.push(Instant::now())
+    }
+
     #[test]
     fn main_test() {
         println!("{:?}", do_push((3,&"hello"), 4.5));
         println!("{:?}", do_pop((3,&"hello")));
         println!("{:?}", do_shift((3,&"hello")));
         println!("{:?}", do_unshift((3,&"hello"), 3));
+
+        let x = ();
+        let x = do_push(x, 1);
+        let x = do_push(x, 2);
+        let x = do_push(x, 3);
+        let x = do_unshift(x,0);
+        let x = do_pop(x);
+        let x = do_shift_unshift(x, 500);
+        let x = do_shift(x);
+        println!("(1,2) = {:?}", x);
+        let x = replace_last_item(x, &"hi");
+        println!("(1,\"hi\") = {:?}", x);
+
+        let x = ();
+        let x = do_unshift(x, 3);
+        let x = do_unshift(x, 2);
+        let x = do_unshift(x, 1);
+        println!("(1,2,3) = {:?}", x);
+
+        let x = push_current_time(x);
+        println!("{:?}", x);
 
         //let y = (1,&"hello",2);
         //println!(TypeId::of::<Us>(), TypeId::of::<(i32,&'static str,i32,)>());
@@ -132,6 +207,25 @@ mod tests {
         println!("{:?}", x);
 
         let x = x.shift().shift();
+        x.apply_to(&|a,b|{ println!("{} {}", a, b); });
+
+
+        let x = |a,b,c| {
+            println!("{} / {} / {}", a, b, c);
+        };
+
+        let y = ();
+        let y = y.push(1);
+        let y = y.push(&"hello");
+        let y = y.push(3.2);
+
+        y.apply_to(&x);
+
+        let x = ();
+        let x = x.push(3);
+        let x = push_current_time(x);
+        let x = x.push(&"hi");
+        let x = push_current_time(x);
         println!("{:?}", x);
     }
 }
